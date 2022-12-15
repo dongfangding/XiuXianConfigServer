e766e9eddf0fe701242da302bbd8176f2f20e311
dongfang.ding <>
2022-12-15 22:02:17 +0800

    feat:初始化

---
+++ xiuxian-core/src/main/java/com/ddf/game/App.java
@@ -0,0 +1,13 @@
+package com.ddf.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
